422a2bd19bbbede652e2c31af18d7851ecebd335
RiftWalker <45465917+RiftWalker6812@users.noreply.github.com>
2025-02-14 11:08:10 -0500

    Update py-02.py
    fixed some issues needs more bug fixing

--- py-02/py-02.py
+++ py-02/py-02.py
@@ -3,8 +3,8 @@ import random
 
 def Gets_A_RANDOM_NUMBER_BETWEEN_THIS_AND_THAT_OVER_THE_WALL_OF_CODE_FALSE_CHAOS(a):
     return random.randint(1, int(a * .5))
-def Gets_A_RANDOM_NUMBER_BETWEEN_THIS_AND_THAT_OVER_THE_WALL_OF_CODE_FALSE_CHAOS(a, b):
-    return random.randint(a, b)
+# def Gets_A_RANDOM_NUMBER_BETWEEN_THIS_AND_THAT_OVER_THE_WALL_OF_CODE_FALSE_CHAOS(a, b):
+#    return random.randint(a, b)
 
 #The initilizating of the game that both rules use just returns a random and another a choice
 def Initilize_Game_of_Nim():
@@ -14,8 +14,7 @@ def Initilize_Game_of_Nim():
     else:
         w = "Player"
     print(f"{w} will go first")
-    p = Gets_A_RANDOM_NUMBER_BETWEEN_THIS_AND_THAT_OVER_THE_WALL_OF_CODE_FALSE_CHAOS(10, 100)
-    
+    p = random.randint(10, 100)
     return w, p
 
 #The Game loop for the game, this is the loop for easy and hard
@@ -27,6 +26,7 @@ def game_loop(pri, defi, pile, turn):
         if turn is "AI":
             print("It is now the AI's turn")
             marbles_taken = defi(pile)
+            pile -= marbles_taken
             print(f"AI pulls {marbles_taken}")
             if pile is 0:
                 print("Player Has Won the game")
@@ -34,43 +34,66 @@ def game_loop(pri, defi, pile, turn):
             turn = "Player"
         elif turn is "Player":
             print(f"It is now the players turn, pick a number between 1 and {int(pile * .5)}")
-            marbles_taken = int(input("Input take: "))
+            while True:
+                try:
+                    marbles_taken = int(input(f"Input take (1 to {int(pile * 0.5)}): "))
+                    if 1 <= marbles_taken <= int(pile * 0.5):
+                        break
+                    else:
+                        print("Invalid move! Choose a number within the allowed range.")
+                except ValueError:
+                        print("Invalid input! Enter a number.")
+            pile -= marbles_taken
+
             print(f"Player take {marbles_taken}")
             if pile is 0:
-                print("Player has LOST!")
+                print("Player has LOST! (╯‵□′)╯︵┻━┻")
                 break
         else:
             print("ERROR")
             exit()
 
+#Easy Mode definition for the game loop     
 def Easy_Mode():
     print("Easy Mode Selected")
     who_goes_First, pile = Initilize_Game_of_Nim()
-    
     game_loop(f"There are {pile} marbles in the pile", Gets_A_RANDOM_NUMBER_BETWEEN_THIS_AND_THAT_OVER_THE_WALL_OF_CODE_FALSE_CHAOS, pile, who_goes_First)
-            
+
+#Hard Mode definition for the game loop            
 def Hard_Mode():
-    def MAKE_IT_HARDER_FOR_THE_PLAYER(pile): #JUST IMAGINE IF I START MAKING ALL OF THE VARIABLES ENIGMATIC
-        n = 1
-        while True:
-            num = (2**n) - 1
-            if num >= pile:
-                break
+    def MAKE_IT_HARDER_FOR_THE_PLAYER(pile): 
+        n, num = 1, 1
+        while (2**n - 1) <= pile:
+            num = (2**n - 1)
             n += 1
         return num    
     
     print("Hard Mode Selected")
     who_The_Turn_IS_IT, pile = Initilize_Game_of_Nim()
-    game_loop("HARD MODE DOESNT GIVE YOU A PILE COUNT, WHY YOU ASK, IM IN COMMAND NOW (SCREECHES)!!!  🙂", MAKE_IT_HARDER_FOR_THE_PLAYER, pile, who_The_Turn_IS_IT)
+    game_loop("The number of marbles in the pile is a mystery 🙂", MAKE_IT_HARDER_FOR_THE_PLAYER, pile, who_The_Turn_IS_IT)
+
+def InvalidOption():
+    print("Invalid selection! Please enter a valid number.")
+
 
 def main():
-    print("🚀")
-    print("Hello, welcome to the game of Nim\nWould you like to play, please select an option below.\n0) Exit\n1) Easy Mode\n2) Hard Mode")
+    option_Selection = {
+        0: exit,
+        1: Easy_Mode,
+        2: Hard_Mode
+    }
     
-    print("PH MENU OPTIONS")
+    print("Starting...")  
+    print("")
     
-print("Starting...")    
-main()  
+    while True:
+        print("🚀 Hello, welcome to the game of Nim 🚀\nWould you like to play, please select an option below.\n0) Exit\n1) Easy Mode\n2) Hard Mode")
+        try:
+            user_Input = int(input("Choose an option: "))
+            option_Selection.get(user_Input, InvalidOption)()
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
+    
 if __name__ == '__main__':
     main()
