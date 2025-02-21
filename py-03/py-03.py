35dbc5d93e6ad57cfd24ce57a0510530474678a3
RiftWalker <45465917+RiftWalker6812@users.noreply.github.com>
2025-02-21 10:44:25 -0500

    couldnt make it with emojis so i went ahead and finalized the code

--- py-03/py-03.py
+++ py-03/py-03.py
@@ -3,10 +3,11 @@ import os
 # Get the directory containing 'library'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-#additional librarys
+# additional librarys
 import time
 from Librarys.RiftLib import is_prime
- 
+
+# Gets Input
 while True:
     try:
         N = int(input("Give me a number between 1 and 100, OR ELSE: "))
@@ -15,14 +16,15 @@ while True:
         break
     except ValueError as e:
         print(f"{e} try again")
-        continue
 
+# Starting time collection
 start_Time = time.time()
 
+# Initial array
 a = [[i for i in range(1, N + 1)], [0] * N]
-print(f"{a[0]}\n{a[1]}")
-print()
+print(f"{a[0]}\n{a[1]}\n")
 
+# Checks for primes
 i = 0
 for i in range(N):
     if is_prime(i):
@@ -30,10 +32,11 @@ for i in range(N):
         if N > i*2:
             a[1][i*2] = 1
 
+# Gets the final variables and ends time calculation
 largest_prime = max([num for num in a[0] if is_prime(num)])
 End_Time = time.time()
 Elapsed_Time = End_Time - start_Time
 
-print(a[0])
-print(a[1])
+# Prints Data
+print(a[0], a[1], sep="\n")
 print(f"The Time it took for the calculations: {Elapsed_Time} Seconds \nThe largest prime is: {largest_prime}")
